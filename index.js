9f1bbf8e90541d52eedbb5221686f0253e2f53fc
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-08-08 07:39:30 -0500

    repairing file path

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-//shows us what going on in teh background
-console.log(arguments)
+//shows us what going on in the background
+// console.log(arguments)
 
 // const message = require('./myscript.js')
-
 // console.log(message)
+
+// const message = require('./myscript.js')
+// console.log(require.cache)
+
+const counterObject = require('./myscript.js')
+
+console.log(counterObject.getCounter())
+counterObject.incrementCounter()
+console.log(counterObject.getCounter())
