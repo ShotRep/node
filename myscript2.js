f32850858fa67871fc84b3fcc6c7f2deeca1c585
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-08-08 07:57:51 -0500

    Trying to fix this project

---
+++ myscript2.js
@@ -0,0 +1,13 @@
+const message = "hi there"
+module.exports = message
+
+let counter = 0
+
+module.exports = {
+  incrementCounter() {
+    counter = counter +1
+  },
+  getCounter() {
+    return counter
+  }
+}
